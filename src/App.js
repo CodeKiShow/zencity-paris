fca050a4d21018532be6799406df62e123b58011
CodeKiShow <135666146+CodeKiShow@users.noreply.github.com>
2024-12-21 12:15:36 +0100

    Initial commit with complete app structure

--- src/App.js
+++ src/App.js
@@ -2,24 +2,3 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import ZenSpots from './components/ZenSpots';
-import RestaurantsView from './components/RestaurantsView';
-import BookingSystem from './components/BookingSystem';
-import ReviewSystem from './components/ReviewSystem';
-import GamificationSystem from './components/GamificationSystem';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<ZenSpots />} />
-        <Route path="/restaurants" element={<RestaurantsView />} />
-        <Route path="/booking" element={<BookingSystem />} />
-        <Route path="/reviews" element={<ReviewSystem />} />
-        <Route path="/profile" element={<GamificationSystem />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
