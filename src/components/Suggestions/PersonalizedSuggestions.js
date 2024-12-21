4b485e962aebdf7a4998201cbf45b63fd6a0e167
CodeKiShow <135666146+CodeKiShow@users.noreply.github.com>
2024-12-21 12:26:11 +0100

    Add enhanced GeoFilter with transport modes and start personalization features

---
+++ src/components/Suggestions/PersonalizedSuggestions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PersonalizedSuggestions = () => {
+  return (
+    <div>Suggestions personnalisées</div>
+  );
+};
+
+export default PersonalizedSuggestions;
---
+++ src/components/ThematicRoutes/CustomRoutes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CustomRoutes = () => {
+  return (
+    <div>Parcours personnalisés</div>
+  );
+};
+
+export default CustomRoutes;
